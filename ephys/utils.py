de7a9708615fadb416d8e137475477d7a76f303d
danielparthier <daniel.parthier@gmail.com>
2025-05-13 17:04:59 +0200

    fix "Any" typing and quotation marks

--- ephys/utils.py
+++ ephys/utils.py
@@ -3,14 +3,14 @@ This module provides utility functions for pattern matching in strings.
 It includes functions to check if elements in a list or numpy array match a
 given pattern.
 """
-
+from typing import Any
 from re import compile as compile_regex
 from matplotlib import colormaps
 from matplotlib.colors import is_color_like
 import numpy as np
 
 
-def color_picker(length: int, index: int, color: str = 'black') -> str | np.ndarray:
+def color_picker(length: int, index: int, color: str = "black") -> str | np.ndarray:
     """
     Selects a color from a colormap or validates a given color string.
 
@@ -32,13 +32,13 @@ def color_picker(length: int, index: int, color: str = 'black') -> str | np.ndar
         pass
     else:
         print("Invalid color. Default to 'viridis'.")
-        color_map = colormaps['viridis']
+        color_map = colormaps["viridis"]
         color = color_map(np.linspace(0, 1, length))[index]
     return color
 
 
 def trace_color(
-    traces: np.ndarray, index: int, color: str = 'black'
+    traces: np.ndarray, index: int, color: str = "black"
 ) -> str | np.ndarray:
     """
     Returns the color for a specific trace in a given colormap.
@@ -54,7 +54,7 @@ def trace_color(
     return color_picker(traces.shape[0], index, color)
 
 
-def string_match(pattern: any, string_list: any) -> np.ndarray:
+def string_match(pattern: Any, string_list: Any) -> np.ndarray:
     """
     Check if the given pattern matches any element in the input list.
 
@@ -78,12 +78,12 @@ def string_match(pattern: any, string_list: any) -> np.ndarray:
     elif isinstance(pattern, np.ndarray):
         pattern = pattern.tolist()
     if not isinstance(pattern, list):
-        raise ValueError('Pattern must be a string, list, or numpy array of strings.')
+        raise ValueError("Pattern must be a string, list, or numpy array of strings.")
     if isinstance(string_list, np.ndarray):
         string_list = string_list.tolist()
     elif isinstance(string_list, str):
         string_list = [string_list]
     if not isinstance(string_list, list):
-        raise ValueError('Input list must be a str, list or numpy array of strings.')
-    pattern_string = '|'.join(pattern)
+        raise ValueError("Input list must be a str, list or numpy array of strings.")
+    pattern_string = "|".join(pattern)
     return np.array([single_match(pattern_string, i) for i in string_list])
