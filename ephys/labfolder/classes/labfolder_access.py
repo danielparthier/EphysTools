c391193dc3428ecff0e727144838e312da935139
danielparthier <daniel.parthier@gmail.com>
2025-05-15 15:57:37 +0200

    add type hints in user info and simplify

--- ephys/labfolder/classes/labfolder_access.py
+++ ephys/labfolder/classes/labfolder_access.py
@@ -3,10 +3,12 @@ This module provides functionality for interacting with the Labfolder API.
 It includes methods for user authentication, data retrieval, and other
 operations related to Labfolder.
 """
+from typing import Any
 import getpass
 import re
 import json
 import requests
+
 
 class LabFolderUserInfo:
     """
@@ -35,31 +37,36 @@ class LabFolderUserInfo:
         Retrieves and sets the user information from Labfolder API.
     """
 
-    def __init__(self, auth_token: dict, labfolder_url: str) -> None:
+    def __init__(self, auth_token: dict = {}, labfolder_url: str = "") -> None:
         self.auth_token = auth_token
         self.labfolder_url = labfolder_url
-        self.API_address = labfolder_url + '/api/v2/'
-        self._get_user_info()
+        self.API_address = labfolder_url + "/api/v2/"
+        if len(auth_token) > 0 and len(labfolder_url) > 0:
+            self._get_user_info()
+        else:
+            print(
+                "No authentication token or Labfolder URL provided. User information not retrieved."
+            )
 
     def _get_user_info(self):
         user_data = requests.get(
-            self.API_address + 'me',
-            params={'expand': 'user'},
+            self.API_address + "me",
+            params={"expand": "user"},
             headers=self.auth_token,
             timeout=5,
         ).json()
-        self.first_name = user_data['user']['first_name']
-        self.last_name = user_data['user']['last_name']
-        self.initials = re.sub(r'[^A-Z]', '', self.first_name) + re.sub(
-            r'[^A-Z]', '', self.last_name
+        self.first_name = user_data["user"]["first_name"]
+        self.last_name = user_data["user"]["last_name"]
+        self.initials = re.sub(r"[^A-Z]", "", self.first_name) + re.sub(
+            r"[^A-Z]", "", self.last_name
         )
-        self.email = user_data['user']['email']
-        self.id = user_data['user']['id']
-        self.location = user_data['user_settings']['zone_id']
+        self.email = user_data["user"]["email"]
+        self.user_id = user_data["user"]["id"]
+        self.location = user_data["user_settings"]["zone_id"]
 
 
 def labfolder_login(
-    labfolder_url: str, user: str = '', password: str = '', allow_input: bool = True
+    labfolder_url: str = "", user: str = "", password: str = "", allow_input: bool = True
 ) -> LabFolderUserInfo:
     """
     Logs into the Labfolder API and returns user information.
@@ -88,47 +95,51 @@ def labfolder_login(
           specifically.
     """
     if allow_input:
-        if user == '':
-            user = input('user email: ')
-        if password == '':
-            password = getpass.getpass('Password for ' + user + ': ')
+        if user == "":
+            user = input("user email: ")
+        if password == "":
+            password = getpass.getpass("Password for " + user + ": ")
 
-    api_url = labfolder_url + '/api/v2/auth/login'
+    api_url = labfolder_url + "/api/v2/auth/login"
 
-    payload = json.dumps({'user': user, 'password': password})
-    headers = {'Content-Type': 'application/json'}
+    payload = json.dumps({"user": user, "password": password})
+    headers = {"Content-Type": "application/json"}
     response = requests.request(
-        'POST', api_url, headers=headers, data=payload, timeout=5
+        "POST", api_url, headers=headers, data=payload, timeout=5
     )
     if response.status_code == 401:
         print(
-            'Username or password incorrect.\nLogin failed. Status code: '
+            "Username or password incorrect.\nLogin failed. Status code: "
             + str(response.status_code)
         )
-        user_auth = None
+        user_auth = LabFolderUserInfo()
     elif response.status_code == 400:
         print(
-            'Incorrect input.\nLogin failed. Status code: ' + str(response.status_code)
+            "Incorrect input.\nLogin failed. Status code: " + str(response.status_code)
         )
-        user_auth = None
+        user_auth = LabFolderUserInfo()
     elif response.status_code == 403:
-        print('Blocked login.\nLogin failed. Status code: ' + str(response.status_code))
-        user_auth = None
+        print("Blocked login.\nLogin failed. Status code: " + str(response.status_code))
+        user_auth = LabFolderUserInfo()
     elif response.status_code == 200:
-        auth_token = {'Authorization': 'Bearer ' + response.json()['token']}
+        auth_token = {"Authorization": "Bearer " + response.json()["token"]}
         user_auth = LabFolderUserInfo(auth_token, labfolder_url)
-        print('Hello ' + user_auth.first_name + '!')
+        print("Hello " + user_auth.first_name + "!")
     else:
-        print('Login failed. Status code: ' + str(response.status_code))
-        user_auth = None
+        print("Login failed. Status code: " + str(response.status_code))
+        user_auth = LabFolderUserInfo()
     return user_auth
 
-def labfolder_logout(user: LabFolderUserInfo) -> any:
-        status = requests.request('POST', user.labfolder_url + '/api/v2/auth/logout',
-                         headers=user.auth_token,
-                         timeout=5)
-        if status.status_code == 204:
-            print('Logout successful')
-            return None
-        else:
-            print('Logout failed. Status code: ' + str(status.status_code))
+
+def labfolder_logout(user: LabFolderUserInfo) -> Any:
+    status = requests.request(
+        "POST",
+        user.labfolder_url + "/api/v2/auth/logout",
+        headers=user.auth_token,
+        timeout=5,
+    )
+    if status.status_code == 204:
+        print("Logout successful")
+        return None
+    else:
+        print("Logout failed. Status code: " + str(status.status_code))
