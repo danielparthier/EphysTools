e3352ec8a9414b03721e51442cdc9acd3ea9b4c6
danielparthier <daniel.parthier@gmail.com>
2025-07-29 10:35:30 +0200

    specify module paths

--- ephys/GUI/meta_data.py
+++ ephys/GUI/meta_data.py
@@ -1,42 +1,57 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QCalendarWidget
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+    QLineEdit,
+    QPushButton,
+    QCalendarWidget,
+)
+from PySide6.QtCore import Qt, QDate
+
 
 class MetaDataWindow(QMainWindow):
-    def __init__(self, on_confirm=None):
+    def __init__(self, on_confirm=None) -> None:
         self.on_confirm = on_confirm
         super().__init__()
         self.setWindowTitle("MetaData")
-        self.setGeometry(100, 100, 400, 300)
-        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
-        # self.setAttribute(Qt.WA_DeleteOnClose)
-        # self.setAttribute(Qt.WA_QuitOnClose, False)
-        # self.setAttribute(Qt.WA_ShowWithoutActivating)
-        # self.setAttribute(Qt.WA_AlwaysShowToolTips)
-        # self.setAttribute(Qt.WA_AlwaysStackOnTop)
-     
+        self.setGeometry(100, 100, 200, 300)
+        self.setMaximumHeight(500)
+        self.setMaximumWidth(300)
+        self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
+
         meta_data_layout = QVBoxLayout()
         # Create a calendar widget
         calendar = QCalendarWidget()
-        calendar.setMaximumWidth(150)
-        calendar.setMaximumHeight(180)
-
+        calendar.setMaximumWidth(180)
+        calendar.setMaximumHeight(190)
         calendar.setGridVisible(True)
-        calendar.setVerticalHeaderFormat(QCalendarWidget.VerticalHeaderFormat.NoVerticalHeader)
-        calendar.setHorizontalHeaderFormat(QCalendarWidget.HorizontalHeaderFormat.SingleLetterDayNames)
-    
-        meta_data_layout.addWidget(calendar)
+        calendar.setVerticalHeaderFormat(
+            QCalendarWidget.VerticalHeaderFormat.NoVerticalHeader
+        )
+        calendar.setHorizontalHeaderFormat(
+            QCalendarWidget.HorizontalHeaderFormat.SingleLetterDayNames
+        )
+
+        btn_today = QPushButton("Today")
+        btn_today.setMaximumWidth(120)
+        btn_today.clicked.connect(lambda: calendar.setSelectedDate(QDate.currentDate()))
+        meta_data_layout.addWidget(btn_today, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        meta_data_layout.addWidget(calendar, alignment=Qt.AlignmentFlag.AlignCenter)
         # add calendar output to the window
-        self.calendar = calendar
+        self.calendar: QCalendarWidget = calendar
 
         # Create a widget to type in experimenter name
         experimenter_name = QLineEdit()
-        experimenter_name.setMaximumWidth(150)
+        # align box to center
+        experimenter_name.setMaximumWidth(200)
         experimenter_name.setPlaceholderText("Experimenter Name")
-        meta_data_layout.addWidget(experimenter_name)
-        self.experimenter_name = experimenter_name
+        meta_data_layout.addWidget(
+            experimenter_name, alignment=Qt.AlignmentFlag.AlignCenter
+        )
+        self.experimenter_name: QLineEdit = experimenter_name
 
         # Set the layout to a central widget so widgets show up
         central_widget = QWidget()
@@ -48,21 +63,21 @@ class MetaDataWindow(QMainWindow):
         confirm_button.setMaximumWidth(150)
         confirm_button.setCheckable(False)
         confirm_button.clicked.connect(self.confirm_and_close)
-        meta_data_layout.addWidget(confirm_button)
+        meta_data_layout.addWidget(
+            confirm_button, alignment=Qt.AlignmentFlag.AlignJustify
+        )
         # add button to close window
         cancel_button = QPushButton("Cancel")
         cancel_button.setMaximumWidth(150)
         cancel_button.setCheckable(False)
         cancel_button.clicked.connect(self.close)
-        meta_data_layout.addWidget(cancel_button)
+        meta_data_layout.addWidget(
+            cancel_button, alignment=Qt.AlignmentFlag.AlignCenter
+        )
         # add button to close window
         self.setLayout(meta_data_layout)
 
-
-    def confirm_and_close(self):
+    def confirm_and_close(self) -> None:
         if self.on_confirm:
-            self.on_confirm(
-                self.experimenter_name.text(),
-                self.calendar.selectedDate()
-            )
+            self.on_confirm(self.experimenter_name.text(), self.calendar.selectedDate())
         self.close()
