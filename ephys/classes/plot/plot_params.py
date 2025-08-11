f18fff336b32b474326f3f465a517667ce6f72e9
danielparthier <daniel.parthier@gmail.com>
2025-08-11 14:53:57 +0200

    switch to theme defaults but update additional inputs

--- ephys/classes/plot/plot_params.py
+++ ephys/classes/plot/plot_params.py
@@ -40,27 +40,29 @@ class PlotParams:
         **kwargs: Any,
     ) -> None:
         """Initialize PlotParams with default values or provided kwargs."""
+        self.theme = kwargs.get("theme", "dark")
+        self.apply_theme(self.theme)
         self.signal_type = kwargs.get("signal_type", "")
         self.channels = kwargs.get("channels", np.array([], dtype=np.int64))
-        self.color = kwargs.get("color", "white")
+        self.color = kwargs.get("color", self.color)
         self.alpha = kwargs.get("alpha", 1.0)
         self.average = kwargs.get("average", False)
         self.avg_color = kwargs.get("avg_color", "red")
         self.align_onset = kwargs.get("align_onset", True)
+        self.cumulative = kwargs.get("cumulative", False)
+        self.stimulus_interval = kwargs.get("stimulus_interval", 0.0)
         self.sweep_subset = kwargs.get("sweep_subset", None)
-        self.bg_color = kwargs.get("bg_color", "black")
-        self.axis_color = kwargs.get("axis_color", "white")
+        self.bg_color = kwargs.get("bg_color", self.bg_color)
+        self.axis_color = kwargs.get("axis_color", self.axis_color)
         self.window = kwargs.get("window", [(0, 0)])
-        self.window_color = kwargs.get("window_color", "gray")
+        self.window_color = kwargs.get("window_color", self.window_color)
         self.xlim = kwargs.get("xlim", (0, 0))
         self.show = kwargs.get("show", True)
         self.return_fig = kwargs.get("return_fig", False)
         self.window_mode = kwargs.get(
             "window_mode", "add_to_trace"
         )  # Default mode for handling windows
-        self.theme = kwargs.get("theme", "dark")
         self.line_width = kwargs.get("line_width", 1.0)
-        self.apply_theme(self.theme)
 
     def apply_theme(self, theme="dark") -> None:
         """Apply the specified theme to the plot parameters.
@@ -74,11 +76,13 @@ class PlotParams:
             self.bg_color = "#292929"
             self.axis_color = "white"
             self.window_color = "#8A8A8A"
+            self.window_color_hover = "#EEEEEE"
         elif self.theme == "light":
             self.color = "#1D1D1DFF"
             self.bg_color = "#FFFFFF"
             self.axis_color = "#000000"
             self.window_color = "#ACACAC"
+            self.window_color_hover = "#5E5E5E"
         else:
             raise ValueError(f"Invalid theme: {self.theme}. Must be 'dark' or 'light'.")
 
@@ -105,7 +109,8 @@ class PlotParams:
         ):
             raise TypeError("window must be a list of tuples (start, end).")
         if (
-            not isinstance(self.xlim, tuple)
+            self.xlim is not None
+            and not isinstance(self.xlim, tuple)
             or len(self.xlim) > 2
             or len(self.xlim) == 1
         ):
@@ -119,6 +124,16 @@ class PlotParams:
                 f"Invalid window_mode: {self.window_mode}. "
                 "Must be 'use_plot', 'use_trace', or 'add_to_trace'."
             )
+        if not isinstance(self.align_onset, bool):
+            raise TypeError("align_onset must be a boolean value.")
+        if not isinstance(self.cumulative, bool):
+            raise TypeError("cumulative must be a boolean value.")
+        if not isinstance(self.stimulus_interval, (int, float)):
+            raise TypeError("stimulus_interval must be a number (int or float).")
+        if not isinstance(self.line_width, (int, float)):
+            raise TypeError("line_width must be a number (int or float).")
+        if not isinstance(self.theme, str) and self.theme not in ["dark", "light"]:
+            raise TypeError("theme must be a string and either 'dark' or 'light'.")
 
     def to_dict(self) -> dict:
         """Convert the plot parameters to a dictionary."""
@@ -130,6 +145,9 @@ class PlotParams:
             "average": self.average,
             "avg_color": self.avg_color,
             "align_onset": self.align_onset,
+            "cumulative": self.cumulative,
+            "stimulus_interval": self.stimulus_interval,
+            "line_width": self.line_width,
             "sweep_subset": self.sweep_subset,
             "bg_color": self.bg_color,
             "axis_color": self.axis_color,
