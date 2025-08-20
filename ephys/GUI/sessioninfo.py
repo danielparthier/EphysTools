400f8976386f69034b7a83325c373ae979f11354
danielparthier <daniel.parthier@gmail.com>
2025-08-20 11:41:27 +0200

    add methods for files

--- ephys/GUI/sessioninfo.py
+++ ephys/GUI/sessioninfo.py
@@ -1,4 +1,5 @@
 import uuid
+import numpy as np
 
 
 class SessionInfo:
@@ -10,3 +11,19 @@ class SessionInfo:
         self.current_user = ""
         self.theme = ""
         self.session_id = str(uuid.uuid4())
+
+    def set_file_list(self, file_list: list[str] | str | np.ndarray) -> None:
+        if isinstance(file_list, str):
+            self.file_list = [file_list]
+        elif isinstance(file_list, np.ndarray):
+            self.file_list = file_list.tolist()
+        else:
+            self.file_list = file_list
+
+    def set_file_path(self, file_path: list[str] | str | np.ndarray) -> None:
+        if isinstance(file_path, str):
+            self.file_path = file_path
+        elif isinstance(file_path, np.ndarray):
+            self.file_path = file_path.tolist()
+        else:
+            self.file_path = file_path
